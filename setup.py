25a7134d1befdde9bc09bd8eeb966a18720c5869
Ethan Smith <ethan@onebytegone.com>
2015-09-07 20:50:46 -0400

    Added basic menu to setup

--- setup.py
+++ setup.py
@@ -2,6 +2,50 @@
 
 import os
 
+
+# ----------------------------------
+# Menu
+# ----------------------------------
+
+def display_menu(options, hasBack = False):
+   # Generate menu items
+   menuItems = []
+   for index, option in enumerate(options):
+      menuItems.append(create_menu_option(str(index + 1), option, False))
+   if hasBack:
+      menuItems.append(create_menu_option("b", "To previous menu", False))
+   menuItems.append(create_menu_option("q", "Quit", False))
+
+   # Display menu to user
+   print "Select an option:"
+   for item in menuItems:
+      print item["entry"]
+
+   # Process response
+   valid = False
+   validOptions = map(lambda item: item["key"], menuItems)
+   while not valid:
+      selected = raw_input("-> ")
+      selected = selected.lower()
+
+      if selected in validOptions:
+         valid = True
+         # Handle response
+      else:
+         print "Invalid option."
+
+def create_menu_option(key, name, action):
+   return {
+      "key": key,
+      "entry": "  %s) %s" % (key, name),
+      "action": action
+   }
+
+
+# ----------------------------------
+# Actions
+# ----------------------------------
+
 def sanitize_path(path):
    return os.path.abspath(os.path.expanduser(path))
 
@@ -15,4 +59,13 @@ def symlink(name, target, config_path, force = False):
    os.symlink(sanitize_path(config_path), linked)
    return True
 
-symlink('.gitconfig', '~', './gitconfig')
+
+# ----------------------------------
+# Runtime
+# ----------------------------------
+
+display_menu([
+   'Run all tasks',
+   'Setup symlinks',
+   'Setup git user'
+]);
