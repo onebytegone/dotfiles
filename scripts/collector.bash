0ee2fb20580efd9316c0c66424f0698d013322c7
Ethan Smith <ethan@onebytegone.com>
2015-09-18 12:03:45 -0400

    Fixed relative path issues

--- scripts/collector.bash
+++ scripts/collector.bash
@@ -1,3 +1,5 @@
-source ./git-completion.bash
-source ./npm-completion.bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+
+source $parent_path/git-completion.bash
+source $parent_path/npm-completion.bash
 
